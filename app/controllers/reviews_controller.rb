b7f96aa32c430f97132ae49fae53d8f1a0675d28
jeannaegelen <jean.naegelen@gmail.com>
2025-03-04 15:59:08 +0100

    changement de création de review depuis software à licenses

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,33 @@
 class ReviewsController < ApplicationController
-
+  before_action :set_license
   def new
     @review = Review.new
+  end
+  def create
+    @review = Review.new(review_params)
+    @review.license = @license
+    @review.user = current_user
+
+    if current_user.license.valid?
+      @review.save
+    else
+      render :new, status: "You have to get a license to comment this software"
+    end
+
+    if @review.valid?
+      redirect_to software_path(@software)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def review_params
     params.require(:review).permit(:rating, :comment)
+  end
+
+  def set_license
+    @license = License.find(params[:license_id])
   end
 end

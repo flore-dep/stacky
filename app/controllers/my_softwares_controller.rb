ba4f264a60d2ee92773b545f75d79ab3a3f7388c
Victor Benhamou <vbenhamou98@gmail.com>
2025-03-04 16:01:16 +0100

    added my-softwares tab and updated software-controller

--- app/controllers/my_softwares_controller.rb
+++ app/controllers/my_softwares_controller.rb
@@ -2,6 +2,6 @@ class MySoftwaresController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @my_softwares = current_user.softwares
+    @my_softwares = Software.where(user_id: current_user.id)
   end
 end

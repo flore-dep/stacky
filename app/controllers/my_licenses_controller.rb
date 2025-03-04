89cf90bd2dafac7e1f2d43b6db8fdf1f2ee78a56
Flore de Paillerets <flore.depaillerets@gmail.com>
2025-03-04 18:09:31 +0100

    license active request sql

--- app/controllers/my_licenses_controller.rb
+++ app/controllers/my_licenses_controller.rb
@@ -2,9 +2,9 @@ class MyLicensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @licenses = License.where(user_id: current_user.id)
+    @licenses_active = License.where("end_at < ? AND user_id = ?", Time.current, current_user.id)
+    @licenses_past = License.where(user_id: current_user.id)
     @softwares = @licenses.map { |license| license.software.id}
     @software_licenses = Software.where(id: @softwares)
-    # hehe
   end
 end

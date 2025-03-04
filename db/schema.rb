970a4d8692a748acea6e1570c90121de97e1c980
Flore de Paillerets <flore.depaillerets@gmail.com>
2025-03-04 11:50:07 +0100

    start front software

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_03_144938) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_04_101005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_03_144938) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "licenses", force: :cascade do |t|
+    t.date "start_at"
+    t.date "end_at"
+    t.bigint "software_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["software_id"], name: "index_licenses_on_software_id"
+    t.index ["user_id"], name: "index_licenses_on_user_id"
   end
 
   create_table "softwares", force: :cascade do |t|
@@ -72,5 +83,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_03_144938) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "licenses", "softwares"
+  add_foreign_key "licenses", "users"
   add_foreign_key "softwares", "users"
 end

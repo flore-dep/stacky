dbf363b8f8de850b6e32a2c47c1d413225448da0
Flore de Paillerets <flore.depaillerets@gmail.com>
2025-03-05 16:35:33 +0100

    active past button scss

--- app/javascript/controllers/past_licenses_controller.js
+++ app/javascript/controllers/past_licenses_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="past-licenses"
 export default class extends Controller {
-  static targets = ["pastlicenses", "activelicenses"]
+  static targets = ["pastlicenses", "activelicenses", "pastbutton"]
 
   connect() {
     console.log("hey");
@@ -10,11 +10,13 @@ export default class extends Controller {
 
   viewpast() {
     this.activelicensesTarget.classList.add("d-none");
+    this.pastbuttonTarget.classList.add("gray-button-active")
     this.pastlicensesTarget.classList.remove("d-none");
   }
 
   viewactive() {
     this.activelicensesTarget.classList.remove("d-none");
+    this.pastbuttonTarget.classList.remove("gray-button-active")
     this.pastlicensesTarget.classList.add("d-none");
   }
 }

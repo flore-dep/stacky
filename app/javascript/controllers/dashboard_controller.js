c4fe46d3035fbae2ca42a6c603ca33b72b652705
jeannaegelen <jean.naegelen@gmail.com>
2025-03-05 18:00:28 +0100

    Javascript is working lancement du dashboard

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -2,23 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dashboard"
 export default class extends Controller {
+  static targets = ["dashboard", "myApps", "dashboardButton", "myAppsButton"]
+
   connect() {
     console.log("Fleur Imichon");
   }
 
   viewDashboard() {
-    this.activebuttonTarget.classList.remove("green-button-visible");
-    this.activebuttonTarget.classList.add("green-button-disable");
-    this.activelicensesTarget.classList.add("d-none");
-    this.pastlicensesTarget.classList.remove("d-none");
-    this.pastbuttonTarget.classList.add("gray-button-active");
+    this.myAppsButtonTarget.classList.remove("green-button-visible");
+    this.myAppsButtonTarget.classList.add("green-button-disable");
+    this.myAppsTarget.classList.add("d-none");
+    this.dashboardTarget.classList.remove("d-none");
+    this.dashboardButtonTarget.classList.add("gray-button-active");
   }
 
   viewApps() {
-    this.pastbuttonTarget.classList.remove("gray-button-active");
-    this.pastlicensesTarget.classList.add("d-none");
-    this.activebuttonTarget.classList.remove("green-button-disable");
-    this.activebuttonTarget.classList.add("green-button-visible");
-    this.activelicensesTarget.classList.remove("d-none");
+    this.dashboardButtonTarget.classList.remove("gray-button-active");
+    this.dashboardTarget.classList.add("d-none");
+    this.myAppsButtonTarget.classList.remove("green-button-disable");
+    this.myAppsButtonTarget.classList.add("green-button-visible");
+    this.myAppsTarget.classList.remove("d-none");
   }
 }

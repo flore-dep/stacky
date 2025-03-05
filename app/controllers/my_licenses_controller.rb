28530d91ec339fe1defe7ce43e08e277031d3a29
Flore de Paillerets <flore.depaillerets@gmail.com>
2025-03-05 12:44:53 +0100

    past/active

--- app/controllers/my_licenses_controller.rb
+++ app/controllers/my_licenses_controller.rb
@@ -2,9 +2,9 @@ class MyLicensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @licenses_active = License.where("end_at < ? AND user_id = ?", Time.current, current_user.id)
-    @licenses_past = License.where(user_id: current_user.id)
-    @softwares = @licenses.map { |license| license.software.id}
-    @software_licenses = Software.where(id: @softwares)
+    @licenses_active = License.active_license_per_user(current_user.id)
+    @licenses_past = License.past_license_per_user(current_user.id)
+    @softwares_active = @licenses_active.map { |license| license.software}
+    @softwares_past = @licenses_past.map { |license| license.software}
   end
 end
